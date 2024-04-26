0549990159bc6d25cc0e7993b72abd1452b03bd0
Vaibhav Kumar Gupta <112247368+kumarguptavaibhav@users.noreply.github.com>
2024-04-26 19:02:31 +0530

    Add files via upload

---
+++ pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/hooman/gitjargan/Posture/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/src/extract_poses.cpp" "CMakeFiles/pose_extractor.dir/src/extract_poses.cpp.o" "gcc" "CMakeFiles/pose_extractor.dir/src/extract_poses.cpp.o.d"
+  "/home/hooman/gitjargan/Posture/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/src/human_pose.cpp" "CMakeFiles/pose_extractor.dir/src/human_pose.cpp.o" "gcc" "CMakeFiles/pose_extractor.dir/src/human_pose.cpp.o.d"
+  "/home/hooman/gitjargan/Posture/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/src/peak.cpp" "CMakeFiles/pose_extractor.dir/src/peak.cpp.o" "gcc" "CMakeFiles/pose_extractor.dir/src/peak.cpp.o.d"
+  "/home/hooman/gitjargan/Posture/lightweight-human-pose-estimation-3d-demo.pytorch/pose_extractor/wrapper.cpp" "CMakeFiles/pose_extractor.dir/wrapper.cpp.o" "gcc" "CMakeFiles/pose_extractor.dir/wrapper.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ pose_extractor/build/tmp/CMakeFiles/pose_extractor.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "../pose_extractor.pdb"
+  "../pose_extractor.so"
+  "CMakeFiles/pose_extractor.dir/src/extract_poses.cpp.o"
+  "CMakeFiles/pose_extractor.dir/src/extract_poses.cpp.o.d"
+  "CMakeFiles/pose_extractor.dir/src/human_pose.cpp.o"
+  "CMakeFiles/pose_extractor.dir/src/human_pose.cpp.o.d"
+  "CMakeFiles/pose_extractor.dir/src/peak.cpp.o"
+  "CMakeFiles/pose_extractor.dir/src/peak.cpp.o.d"
+  "CMakeFiles/pose_extractor.dir/wrapper.cpp.o"
+  "CMakeFiles/pose_extractor.dir/wrapper.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pose_extractor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
